377d99e5c1daff46a146aa42240c3ef960010b3a
Nadège <nadegeduflo@hotmail.fr>
2020-11-16 17:15:17 +0100

    finalisation de JS

--- app.js
+++ app.js
@@ -1,9 +1,74 @@
 const field = document.querySelector('input');
+const form = document.querySelector('form');
+const liste = document.querySelector('ul');
+const task = document.querySelector('.task')
+let toutesLesTaches = [];
 
-field.addEventListener('input', (e) =>{
+// animation input
+
+field.addEventListener('input', (e) => {
     if(e.target.value !=""){
-        console.log(e.target);
-        console.log(e.target.value);
-        e.target.parentNode.classList.add('animation')
+        e.target.parentNode.classList.add('animation');
+    } else if (e.target.value ==""){
+        e.target.parentNode.classList.remove('animation');
     }
-})
+});
+
+// créer la liste
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    task.className = "task";
+    
+    const txt = field.value.trim();
+    
+    if(txt !== ""){
+        rajouterUneTache(txt);
+        field.value = "";
+    }
+});
+
+const rajouterUneTache = (tache) =>{
+    const todo = {
+        tache,
+        id: Date.now()
+      }
+    afficherListe(todo);   
+}
+const afficherListe = (todo)=>{
+    const item = document.createElement('li');
+    item.setAttribute('data-key',todo.id);
+    
+    const check = document.createElement('input');
+    check.setAttribute('type','checkbox');
+    check.addEventListener('click',tachefaite);
+    item.appendChild(check);
+    
+    const texte = document.createElement('span');
+    texte.innerText =todo.tache;
+    item.appendChild(texte);
+    
+    const btn = document.createElement('button');
+    btn.addEventListener('click',fermerTache);
+    const img = document.createElement('img');
+    img.setAttribute('src','./ressources/fermer.svg');
+    btn.appendChild(img);
+    item.appendChild(btn);
+
+    liste.appendChild(item)
+    toutesLesTaches.push(item);
+        
+}
+
+const tachefaite = (e)=>{
+    e.target.parentNode.classList.toggle('finDeTache');
+}
+const fermerTache = (e)=>{
+    toutesLesTaches.forEach(el =>{
+        if(e.target.parentNode.getAttribute('data-key')=== el.getAttribute('data-key')){
+            console.log(e.target.parentNode);
+            el.remove();
+            toutesLesTaches.filter(li =>li.dataset.key !== el.dataset.key);
+        }
+    })
+}
